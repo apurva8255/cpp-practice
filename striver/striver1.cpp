5865d1b31aa558ea0141504bbec4219de0123750
apurva8255 <maahisiddhawar@gmail.com>
2025-09-11 20:56:51 +0530

    striver

---
+++ striver/striver1.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+// 1 d array
+  // int arr[5];
+  // cin >> arr[0]>>arr[1]>> arr[2]>>arr[3]>>arr[4];
+  // cout << arr[0] << arr[3] ;
+
+  // //2D array
+  // int arr[3][5]; // [rows] [cols]
+
+  // string s = "apurva";
+  // int len = s.size();//to find length of string
+  // s(len-1) = 'z';
+  // cout << s(len-1);
+
+  // for(int i=1; i<=10; i = i+1){
+  //   cout<<"apurva"<<endl;
+  // }
+
+  // int i;
+  // for(int i=5; i>0; i = i-1){
+  //   cout<<"apurva"<< i <<endl;
+  // // }
+  // cout << i <<endl;
+
+  // int i ;
+  // while(condition){
+
+  //   increment
+  // }
+
+  // int i = 2;
+  // do {
+  //   cout<<"apurva"<< i << endl;
+  //   i++;
+  // } while(i<=1);
+
+  //functions
+
+  //voi
+  //return
+  //parameterised
+  //non parameterised
+
+  // string name;
+  // cin>>name;
+  // printname(name);
+
+  // string name2;
+  // cin>>name2;
+  // printname(name2);
+
+// void printname(string name){
+//     cout<<"hey"<<name;
+//   }
+
+// int sum(int num1, int num2){
+//   int num3= num1+num2;
+//   return num3;
+// }
+
+// int main(){
+//   int num1, num2;
+//   cin>>num1>>num2;
+//   int res = sum(num1, num2);
+//   cout << res;
+
+int main(){
+
+  return 0;
+}
